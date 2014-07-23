69975428fa9bc48cda171da7f0d7e3417e1aa13f
dkester3 <Daniel.Kester@gtri.gatech.edu>
2014-07-23 12:12:37 -0400

    Pitch changing ambient channel

--- CyclingHelper/Assets/Scripts/BehindDetection.cs
+++ CyclingHelper/Assets/Scripts/BehindDetection.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts
 	public class BehindDetection : MonoBehaviour 
     {
         public GameObject audioSrcObj;
-
+		public AudioSource ambientSound;
 		private AudioSource alertSound;
         private HashSet<GameObject> cars;
         private GameObject player;
@@ -33,11 +33,13 @@ namespace Assets.Scripts
 
                 c.UpdateSpeedMultiplier(closestCarDistance < 15 ? closestCarDistance.Remap(0, 15, 0, 1) : 1);
                 alertSound.pitch = closestCarDistance >= 15 ? .1f : 1.4f - closestCarDistance.Remap(0, 15, 0.5f, 1.3f);
-            }
+				ambientSound.pitch = cars.Count > 4 ? 1.5f : ((float) cars.Count).Remap(0f, 4f, .5f, 1.5f);
+			}
             else
             {
                 alertSound.pitch = .1f;
-            }
+				ambientSound.pitch = .5f;
+			}
 		}
 
         float DistToPlayer(GameObject car)
